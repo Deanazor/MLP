53b33f00c0a0ad7a9d60d390e8534f40bd0df2c2
Deanazor <arief.p.a@mail.ugm.ac.id>
2021-05-06 11:28:46 +0700

    Added output layer

--- layers.py
+++ layers.py
@@ -1,5 +1,7 @@
+from activations import softmax
 import numpy as np
-from activations import sigmoid, relu, softmax
+from activations import softmax
+from losses import CategoricalCrossEntropy
 
 class Dense():
     def __init__(self, ins, outs):
@@ -16,3 +18,23 @@ class Dense():
         self.dweights = np.dot(self.inputs.T, dinputs)
         self.dbias = np.sum(dinputs, axis=0, keepdims=True)
         self.dinputs =  np.dot(dinputs, self.weights.T)
+
+class Output():
+    def __init__(self):
+        self.activation = softmax()
+        self.loss = CategoricalCrossEntropy()
+
+    def forward(self, inputs, actual):
+        self.activation.forward(inputs)
+        self.outputs = self.activation.outputs
+        return self.loss.calc_loss(self.outputs, actual)
+    
+    def backward(self, dinputs, actual):
+        samples = len(dinputs)
+
+        if len(actual.shape) == 2:
+            actual = np.argmax(actual, axis=1)
+        
+        self.dinputs = dinputs.copy()
+        self.dinputs[range(samples), actual] -= 1
+        self.dinputs = self.dinputs / samples
