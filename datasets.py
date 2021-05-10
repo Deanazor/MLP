5eb1e8677fae8988dd255f00ac8241e70bdf19f2
Deanazor <arief.p.a@mail.ugm.ac.id>
2021-05-10 21:54:58 +0700

    added load data

--- datasets.py
+++ datasets.py
@@ -1,4 +1,7 @@
 import numpy as np
+import os
+from PIL import Image
+import cv2
 
 def create_spiral_data(samples = 10, classes=2, dimension=2):
     X = np.zeros((samples*classes, dimension))
@@ -23,3 +26,32 @@ def one_hot_encode(y):
         categorical[i, label] = 1
 
     return categorical
+
+def grayscale(img):
+    bgr_weights = np.array([.1140, .5870, .2989])
+    gray_img = np.dot(img, bgr_weights)
+    return gray_img
+
+def load_from_folder(folder, test_split=0, target_size=None):
+    classes = os.listdir(folder)
+    train_images = []
+    test_images = []
+    for cls in classes:
+        images = []
+        label = classes.index(cls)
+        cls_path = os.path.join(folder, cls)
+        for img_name in os.listdir(cls_path):
+            img_path = os.path.join(cls_path, img_name)
+            img_arr = cv2.imread(img_path)
+            img_arr = cv2.resize(img_arr, target_size,interpolation=cv2.INTER_CUBIC)
+            img_arr = grayscale(img_arr)
+            images.append([img_arr, label])
+        if test_split: 
+            test_size = int(len(images) * (1-test_split))
+            train_images += images[:test_size]
+            test_images += images[test_size:]
+        else :
+            train_images += images
+    if test_split:
+        return train_images, test_images
+    return train_images
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-import numpy as np
-from models import MLP
-from optimizers import SGD
-from losses import CategoricalCrossEntropy
-from datasets import create_spiral_data, one_hot_encode
+# import numpy as np
+# from models import MLP
+# from optimizers import SGD
+# from losses import CategoricalCrossEntropy
+# from datasets import create_spiral_data, one_hot_encode
+# from datasets import load_from_folder
 
-np.random.seed(69)
+# np.random.seed(69)
 
-X,y = create_spiral_data(samples=100, classes=2)
-y = one_hot_encode(y)
+# path = './flowers'
 
-model = MLP(2, 2)
+# X,y = create_spiral_data(samples=100, classes=2)
+# y = one_hot_encode(y)
 
-opt = SGD(lr = 0.1)
-loss = CategoricalCrossEntropy()
+# model = MLP(2, 2)
 
-model.compile(opt, loss)
+# opt = SGD(lr = 0.1)
+# loss = CategoricalCrossEntropy()
 
-model.train(X, y, epochs=100)
+# model.compile(opt, loss)
+
+# model.train(X, y, epochs=100)
+
+# train, test = load_from_folder(path, test_split=0.2)
+a = list(map(int, input().split()))
+x = int(input())
+
+print([a[i:i+x] for i in range(len(a)-x+1)])
